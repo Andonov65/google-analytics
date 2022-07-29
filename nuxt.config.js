3cfc301856fff43da539388eb4d92959c62cfbd9
Marko Andonov <marko.20112000@gmail.com>
2022-07-29 13:30:23 +0200

    g-tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  googleAnalytics: {
-    id: 'G-X96BB0L9GK'
-  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'google-analytics',
@@ -27,6 +24,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '@/plugins/vue-gtag'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -34,7 +32,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/google-analytics'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
